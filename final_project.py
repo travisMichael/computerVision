c5cff44f2d20b5c57d6885175c1a597f501e79e6
Latzke, Travis-CW <Travis.Latzke@bestbuy.com>
2020-04-12 14:34:08 -0500

    final project updates

--- final_project.py
+++ final_project.py
@@ -1,22 +1,20 @@
 from removeObject import *
+from marker import *
 
 
-def remove_object_from_image_bridge():
-    Phi = np.load("bridge/in/Sarah_marker.npy")
-    original = cv2.imread("bridge/in/sarah_r.jpg").astype(float)
-    marker = cv2.imread("bridge/in/Sarah_marker2.jpg").astype(float)
+def remove_object_from_image_shower():
+    original = cv2.imread("bridge/in/shower_r.jpg").astype(float)
+    marker, Phi = get_algorithm_params_for_shower()
     image = set_image(marker, original, Phi)
     confidence_matrix = initialize_confidence_matrix(Phi)
     border = initialize_border(Phi, remove_isolated_pixels=True)
-    template_size = 11
-
+    template_size = 7
     run_algorithm(image, Phi, border, confidence_matrix, template_size, "bridge/out/sarah.jpg")
 
 
 def remove_object_from_image_anne():
-    Phi = np.load("anne/in/anne_marker.npy")
     original = cv2.imread("anne/in/anne_r.jpg").astype(float)
-    marker = cv2.imread("anne/in/anne_marker2.jpg").astype(float)
+    marker, Phi = get_algorithm_params_for_anne()
     image = set_image(marker, original, Phi)
     confidence_matrix = initialize_confidence_matrix(Phi)
     border = initialize_border(Phi, remove_isolated_pixels=True)
@@ -26,9 +24,8 @@ def remove_object_from_image_anne():
 
 
 def remove_object_from_image_island():
-    Phi = np.load("island/in/island_marker.npy")
     original = cv2.imread("island/in/island_r.jpg").astype(float)
-    marker = cv2.imread("island/in/island_marker2.jpg").astype(float)
+    marker, Phi = get_algorithm_params_for_island()
     image = set_image(marker, original, Phi)
     confidence_matrix = initialize_confidence_matrix(Phi)
     border = initialize_border(Phi, remove_isolated_pixels=True)
@@ -39,35 +36,23 @@ def remove_object_from_image_island():
 
 def run_algorithm(image, Phi, border, confidence_matrix, template_size, out_file):
     original_Phi = np.copy(Phi)
-    np.save("original_phi", Phi)
-    # confidence_matrix = np.load("confidence_states/c_229.npy")
-    # Phi = np.load("Phi_states/Phi_229.npy")
-    # original_Phi = np.load("Phi_states/Phi_229.npy")
-    # border = np.load("border.npy")
-    # image = np.load("image_states/beach_patch_filled229.npy").astype(float)
-    # cv2.circle(image, (patch_column, patch_row), 2, [0, 0, 255], thickness=2)
+
     for index in range(0, 5000):
         confidence_matrix, image, Phi, border = patch_iteration(image, Phi, original_Phi, border, confidence_matrix, template_size)
         if index % 10 == 0:
             print(index)
-            # np.save("border", border)
-            # np.save("image_states/beach_patch_filled" + str(index), image)
             cv2.imwrite("image_states/patch_filled" + str(index) + ".jpg", image)
-            # np.save("confidence_states/c_" + str(index), confidence_matrix)
-            # np.save("Phi_states/Phi_" + str(index), Phi)
+
         border_points = np.where(border == 255)
         if len(border_points[0]) < 1:
             break
 
     cv2.imwrite(out_file, image)
-    print("done")
 
 
 if __name__ == "__main__":
     print("hello")
 
-    remove_object_from_image_bridge()
+    remove_object_from_image_shower()
 
-
-
-print("done")
+    print("done")
--- marker.py
+++ marker.py
@@ -2,10 +2,72 @@ import cv2
 import numpy as np
 
 
-def get_image():
-    marker = "bridge/in/Sarah_marker"
-    image = cv2.imread(marker + ".jpg")
-    image = cv2.resize(image, (750, 1000))
+# def get_algorithm_params():
+# #     marker = "bridge/in/Shower_marker"
+# #     image = cv2.imread(marker + ".jpg")
+# #     image = cv2.resize(image, (750, 1000))
+# #
+# #     h, w, _ = image.shape
+# #     im_copy = np.copy(image)
+# #     m_0 = cv2.medianBlur(image[:, :, 0], 1)
+# #     m_1 = cv2.medianBlur(image[:, :, 0], 1)
+# #     m_2 = cv2.medianBlur(image[:, :, 2], 1)
+# #
+# #     zero = im_copy[:, :, 0]
+# #     one = im_copy[:, :, 1]
+# #     two = im_copy[:, :, 2]
+# #
+# #     cond = np.logical_and(m_2 > 200, m_1<50)
+# #     cond = np.logical_and(cond, m_0 < 50)
+# #
+# #     zero[np.where(cond)] = 0
+# #     one[np.where(cond)] = 0
+# #     two[np.where(cond)] = 255
+# #
+# #     np.save(marker, im_copy)
+# #     cv2.imwrite(marker + "2.jpg", im_copy)
+# #
+# #     Phi = np.zeros((h, w))
+# #     Phi[:,:] = 255
+# #     Phi[np.where(cond)] = 0
+# #
+# #     np.save(marker, Phi)
+# #     cv2.imwrite("Phi.jpg", Phi)
+# #     return image
+# get_algorithm_params()
+
+# image = cv2.imread("bridge/in/Shower.jpg")
+# image = cv2.resize(image, (750, 1000))
+# cv2.imwrite("bridge/in/shower_r.jpg", image)
+def get_algorithm_params_for_shower():
+    image = cv2.imread("bridge/in/Shower_marker2.jpg")
+
+    h, w, _ = image.shape
+    im_copy = np.copy(image)
+    m_0 = cv2.medianBlur(image[:, :, 0], 1)
+    m_1 = cv2.medianBlur(image[:, :, 0], 1)
+    m_2 = cv2.medianBlur(image[:, :, 2], 1)
+
+    zero = im_copy[:, :, 0]
+    one = im_copy[:, :, 1]
+    two = im_copy[:, :, 2]
+
+    cond = np.logical_and(m_2 > 200, m_1<50)
+    cond = np.logical_and(cond, m_0 < 50)
+
+    zero[np.where(cond)] = 0
+    one[np.where(cond)] = 0
+    two[np.where(cond)] = 255
+
+    Phi = np.zeros((h, w))
+    Phi[:,:] = 255
+    Phi[np.where(cond)] = 0
+
+    return im_copy, Phi
+
+
+def get_algorithm_params_for_anne():
+    image = cv2.imread("anne/in/anne_marker2.jpg")
 
     h, w, _ = image.shape
     im_copy = np.copy(image)
@@ -24,19 +86,35 @@ def get_image():
     one[np.where(cond)] = 0
     two[np.where(cond)] = 255
 
-    np.save(marker, im_copy)
-    cv2.imwrite(marker + "2.jpg", im_copy)
+    Phi = np.zeros((h, w))
+    Phi[:,:] = 255
+    Phi[np.where(cond)] = 0
+
+    return im_copy, Phi
+
+
+def get_algorithm_params_for_island():
+    image = cv2.imread("island/in/island_marker2.jpg")
+
+    h, w, _ = image.shape
+    im_copy = np.copy(image)
+    m_0 = cv2.medianBlur(image[:, :, 0], 9)
+    m_1 = cv2.medianBlur(image[:, :, 0], 9)
+    m_2 = cv2.medianBlur(image[:, :, 2], 9)
+
+    zero = im_copy[:, :, 0]
+    one = im_copy[:, :, 1]
+    two = im_copy[:, :, 2]
+
+    cond = np.logical_and(m_2 > 200, m_1<50)
+    cond = np.logical_and(cond, m_0 < 50)
+
+    zero[np.where(cond)] = 0
+    one[np.where(cond)] = 0
+    two[np.where(cond)] = 255
 
     Phi = np.zeros((h, w))
     Phi[:,:] = 255
     Phi[np.where(cond)] = 0
 
-    np.save(marker, Phi)
-    cv2.imwrite("Phi.jpg", Phi)
-    return image
-
-get_image()
-
-# image = cv2.imread("bridge/in/Sarah.jpg")
-# image = cv2.resize(image, (750, 1000))
-# cv2.imwrite("bridge/in/sarah_r.jpg", image)
+    return im_copy, Phi
