f6da1206184d7716135ba4ba310ec4098077c8af
tlatzke01 <tlatzke01@gmail.com>
2020-11-23 16:38:42 -0600

    Added testing for new dataset

---
+++ stereo_correspondance/analyze.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+
+
+def compare_with_ground_truth(ground_truth, result, ratio, match):
+    h = ground_truth.shape[0]
+    w = ground_truth.shape[1]
+    ground_truth *= ratio
+
+    diff = np.abs(ground_truth - result)
+
+    matching_disparities = np.where(diff <= 1.9)[0].shape[0]
+
+    p = np.zeros((h,w))
+    p[diff <= 1.9] = 255
+    cv2.imwrite("output/" + match + ".png", p)
+
+    ratio = float(matching_disparities) / float(h*w)
+
+    return ratio
+
+
+def analyze_2_a():
+    ground_truth = cv2.imread("input_images/cones/disp6.png", 0).astype(np.float) / 4.0
+    ssd_result = cv2.imread("output/disparity_ssd_map_2_a.png", 0).astype(np.float) / 9.0
+
+    ground_truth = cv2.resize(ground_truth, (225, 190))
+
+    ground_truth = ground_truth[:, 0:200]
+    ssd_result = ssd_result[:, 0:200]
+
+    ratio_1 = compare_with_ground_truth(ground_truth, ssd_result, 1.0/2.0, "match_2_a")
+    print(ratio_1)
+
+
+def analyze_2_b():
+    ground_truth = cv2.imread("input_images/cones/disp6.png", 0).astype(np.float) / 4.0
+    # ssd_result = cv2.imread("output/disparity/d_27.png", 0).astype(np.float) / 9.0
+    ssd_result = cv2.imread("d_21.png", 0).astype(np.float) / 9.0
+    ground_truth = cv2.resize(ground_truth, (225, 190))
+
+    ground_truth = ground_truth[:, 0:200]
+    ssd_result = ssd_result[:, 0:200]
+
+    ratio_1 = compare_with_ground_truth(ground_truth, ssd_result, 1.0/2.0, "match_2_b")
+    print(ratio_1)
+
+
+if __name__ == '__main__':
+    # analyze_2_a()
+    analyze_2_b()
--- stereo_correspondance/experiment.py
+++ stereo_correspondance/experiment.py
@@ -12,11 +12,15 @@ def test_1():
 
     cv2.imwrite("output/left.png", left)
     cv2.imwrite("output/right.png", right)
-    disparity_map = stereo.pairwise_stereo(left, right, None, method="ssd")
-    print("hello")
+    disparity_map = stereo.pairwise_stereo_ssd(left, right, 3)
+
+    disparity_map[disparity_map > 60] = 0
+    disparity_map *= 4
+
+    cv2.imwrite("output/disparity_ssd_map_1_a.png", disparity_map)
 
 
-def test_2():
+def test_1_b():
     left = cv2.imread("input_images/Couch-perfect/im1.png")
     right = cv2.imread("input_images/Couch-perfect/im0.png")
 
@@ -24,9 +28,14 @@ def test_2():
     right = cv2.resize(right, (200, 200))
     # labels = np.array([5, 10, 15, 20, 25, 30, 35, 40, 45, ])
     # labels = np.array([6, 12, 18, 24, 30, 36, 42, 48, 54])
-    labels = np.array([5, 13, 20, 28, 35, 43, 50, 57])
+    labels = np.array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+                       20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+                       30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+                       40, 41, 42, 43, 44, 45, 46, 47, 48, 49])
+    lambda_v = 5
+    d_thresh = 80
 
-    disparity_map = stereo.pairwise_stereo(left, right, labels, method="graph_cut")
+    disparity_map = stereo.pairwise_stereo_graph_cut(left, right, labels, lambda_v, d_thresh)
     print("hello")
 
 
@@ -34,59 +43,62 @@ def test_2_a():
     left = cv2.imread("input_images/cones/im6.png", 0)
     right = cv2.imread("input_images/cones/im2.png", 0)
 
-    left = cv2.resize(left, (200, 200))
-    right = cv2.resize(right, (200, 200))
+    left = cv2.resize(left, (225, 190))
+    right = cv2.resize(right, (225, 190))
 
     cv2.imwrite("output/left.png", left)
     cv2.imwrite("output/right.png", right)
-    disparity_map = stereo.pairwise_stereo(left, right, None, method="ssd")
-    print("hello")
+    disparity_map = stereo.pairwise_stereo_ssd(left, right, 7)
 
+    disparity_map[disparity_map > 28] = 0
+    disparity_map *= 9
 
-# def test_2_b():
-#     left = cv2.imread("input_images/cones/im6.png")
-#     right = cv2.imread("input_images/cones/im2.png")
-#
-#     left = cv2.resize(left, (225, 190))
-#     right = cv2.resize(right, (225, 190))
-#     # labels = np.array([10, 20, 30, 40, 50, 60])
-#     # labels = np.array([9, 14, 19, 24, 29, 34, 40])
-#     # labels = np.array([9, 12, 15, 18, 21, 24, 27, 30])
-#     labels = np.array([7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26, 27])
-#     # labels = np.array([9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29])
-#     # labels = np.array([8, 13, 17, 21, 25, 29])
-#     # labels = np.array([10, 20, 30, 40, 50, 60, 70])
-#     increment = 1
-#     k = 0.5
-#     k_not = 0.5
-#     intensity_thresh = 20
-#     d_thresh = 20
-#
-#     disparity_map = stereo.pairwise_stereo_graph_cut(left, right, labels, increment, k, k_not, intensity_thresh, d_thresh)
-#     print("hello")
+    cv2.imwrite("output/disparity_ssd_map_2_a.png", disparity_map)
+
 
 def test_2_b():
     left = cv2.imread("input_images/cones/im6.png")
     right = cv2.imread("input_images/cones/im2.png")
+    print(left.shape)
 
     left = cv2.resize(left, (225, 190))
     right = cv2.resize(right, (225, 190))
-    # labels = np.array([10, 20, 30, 40, 50, 60])
-    # labels = np.array([9, 14, 19, 24, 29, 34, 40])
-    # labels = np.array([9, 12, 15, 18, 21, 24, 27, 30])
     labels = np.array([9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26])
-    # labels = np.array([9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29])
-    # labels = np.array([8, 13, 17, 21, 25, 29])
-    # labels = np.array([10, 20, 30, 40, 50, 60, 70])
-    lambda_v = 5
+    lambda_v = 3
     d_thresh = 80
 
-    disparity_map = stereo.pairwise_stereo_graph_cut_3(left, right, labels, lambda_v, d_thresh)
-    print("hello")
+    disparity_map = stereo.pairwise_stereo_graph_cut(left, right, labels, lambda_v, d_thresh)
+    cv2.imwrite("output/disparity_ssd_map_2_b.png", disparity_map)
 
+
+def test_3_b():
+    left = cv2.imread("input_images/tsukuba/scene1.row3.col3.ppm")
+    right = cv2.imread("input_images/tsukuba/scene1.row3.col1.ppm")
+    print(left.shape)
+
+    # left = cv2.resize(left, (225, 190))
+    # right = cv2.resize(right, (225, 190))
+    labels = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17,18,19,20])
+    lambda_v = 2
+    d_thresh = 80
+
+    disparity_map = stereo.pairwise_stereo_graph_cut(left, right, labels, lambda_v, d_thresh)
+    cv2.imwrite("output/disparity_ssd_map_3_b.png", disparity_map)
+
+
+def t():
+    d = cv2.imread("output/disparity_ssd_map_2_b.png")
+    d[d > 28] = 0
+    d *= 9
+    cv2.imwrite("d_6.png",d)
+
+
+    print()
 
 if __name__ == '__main__':
+    # t()
     # test_1()
-    # test_2()
+    # test_1_b()
     # test_2_a()
-    test_2_b()
+    # test_2_b()
+    test_3_b()
