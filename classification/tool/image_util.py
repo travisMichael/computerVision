996626dc6faa9fe0c36308e8e1dbd56b1dfd1881
tlatzke01 <tlatzke01@gmail.com>
2021-11-07 18:12:18 -0600

    Added grey scale image loader

--- classification/tool/image_util.py
+++ classification/tool/image_util.py
@@ -9,3 +9,13 @@ def load(file, height=800):
     new_w = int(new_h*ratio)
     img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
     return img
+
+
+def load_grey(file, height=800):
+    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+    h, w = img.shape[:2]
+    ratio = w/float(h)
+    new_h = height
+    new_w = int(new_h*ratio)
+    img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
+    return img
